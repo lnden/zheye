1a373f1b41718236e79a2e758ab413cf9eb47eaa
lnden <258231674@qq.com>
2021-07-28 18:06:40 +0800

    feat: develop column list and global header components

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,68 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <global-header :user="user" />
+    <column-list :list="list"/>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
+
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'test1的专栏',
+    description: '这是test1专栏，有一段非常有意思的简介，可以更新一下哦',
+    avatar: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=4131746241,2477555401&fm=11&gp=0.jpg'
+  },
+  {
+    id: 2,
+    title: 'test2的专栏',
+    description: '这是test2专栏，有一段非常有意思的简介，可以更新一下哦'
+  },
+  {
+    id: 3,
+    title: 'test3的专栏',
+    description: '这是test4专栏，有一段非常有意思的简介，可以更新一下哦',
+    avatar: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=4131746241,2477555401&fm=11&gp=0.jpg'
+  },
+  {
+    id: 4,
+    title: 'test4的专栏',
+    description: '这是test4专栏，有一段非常有意思的简介，可以更新一下哦',
+    avatar: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=4131746241,2477555401&fm=11&gp=0.jpg'
+  },
+  {
+    id: 5,
+    title: 'test5的专栏',
+    description: '这是test5专栏，有一段非常有意思的简介，可以更新一下哦',
+    avatar: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=4131746241,2477555401&fm=11&gp=0.jpg'
+  }
+]
+
+const currentUser: UserProps = {
+  isLogin: true,
+  name: 'Lily'
+}
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    ColumnList,
+    GlobalHeader
+  },
+  setup () {
+    return {
+      list: testData,
+      user: currentUser
+    }
   }
 })
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Dropdown.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="dropdown" ref="dropdonwnRef">
+    <a class="btn btn-outline-light my-2 dropdown-toggle" href="#" @click.prevent="handleToggle">{{title}}</a>
+    <ul class="dropdown-menu" :style="{display: 'block'}" v-if="isOpen">
+      <slot></slot>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, watch } from 'vue'
+import useClickOutside from '../hooks/useClickOutside'
+
+export default defineComponent({
+  name: 'dropdown',
+  props: {
+    title: {
+      type: String,
+      require: true
+    }
+  },
+  setup () {
+    const isOpen = ref(false)
+    const dropdonwnRef = ref<null | HTMLElement>(null)
+
+    const handleToggle = () => {
+      isOpen.value = !isOpen.value
+    }
+
+    const isClickOutside = useClickOutside(dropdonwnRef)
+
+    watch(isClickOutside, () => {
+      if (isOpen.value && isClickOutside.value) {
+        isOpen.value = false
+      }
+    })
+    return {
+      isOpen,
+      handleToggle,
+      dropdonwnRef //* template name
+    }
+  }
+})
+</script>
