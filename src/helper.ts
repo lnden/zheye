1961e22d4deb02b35f0cb1ed2fa7cb56890940ff
lnden <258231674@qq.com>
2021-08-08 14:29:04 +0800

    perf: data transform func

--- src/helper.ts
+++ src/helper.ts
@@ -37,3 +37,60 @@ export function beforeUploadCheck (file: File, condition: CheckCondition): Resul
     error
   }
 }
+
+interface TestProps {
+  _id: string
+  name: string
+}
+const testData: TestProps[] = [
+  { _id: '1', name: 'a' },
+  { _id: '2', name: 'b' }
+]
+
+/**
+ * @description 格式转换 Array => Object
+ * @param Array
+ *  [
+ *    { _id: '1', name: 'a' },
+ *    { _id: '2', name: 'b' }
+ *  ]
+ *
+ * @returns Object
+ *  {
+ *    1: { _id: '1', name: 'a' },
+ *    2: { _id: '2', name: 'b' }
+ *  }
+ */
+export const arrToObj = <T extends { _id?: string }>(arr: Array<T>): { [key: string]: T } => {
+  return arr.reduce((prev, current) => {
+    if (current._id) {
+      prev[current._id] = current
+    }
+    return prev
+  }, {} as { [key: string]: T })
+}
+const result = arrToObj(testData)
+console.log('转换的结果', result)
+
+/**
+ * @description 格式转换 Object => Array
+ * @param Object
+ *  {
+ *    1: { _id: '1', name: 'a' },
+ *    2: { _id: '2', name: 'b' }
+ *  }
+ * @returns Array
+ *  [
+ *    { _id: '1', name: 'a' },
+ *    { _id: '2', name: 'b' }
+ *  ]
+ */
+export const objToArr = <T>(obj: {[key: string]: T}): T[] => {
+  return Object.keys(obj).map(key => obj[key])
+}
+const testData2: { [key: string]:TestProps } = {
+  1: { _id: '1', name: 'a' },
+  2: { _id: '2', name: 'b' }
+}
+const result2 = objToArr(testData2)
+console.log('转换的结果', result2)
